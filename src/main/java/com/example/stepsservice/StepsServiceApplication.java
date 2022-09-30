001aa5f23b8a50abf7329a36ca5f886dcc09f8a5
Mathias <r0618721@student.thomasmore.be>
2022-09-30 13:55:15 +0200

    first commit

---
+++ src/main/java/com/example/stepsservice/StepsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stepsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stepsservice/StepsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
